ccff54d39bd16395f1f715462b9deb8db4669496
Cihat KOÇAK <pcihatkocakks@gmail.com>
2022-01-21 18:28:56 +0300

    Typography-Examp-ok

--- make-styles/src/App.js
+++ make-styles/src/App.js
@@ -1,15 +1,34 @@
 import React from "react";
 import Coolbutton from "./Coolbutton";
-import Buttons from "./Buttons"
+import Buttons from "./Buttons";
+import Typographyexapmles from "./Typographyexapmles";
 import { Grid } from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { green, purple } from "@mui/material/colors";
 
 export default function App() {
   const cool = false;
   //Get Style Example --> CoolButton
+
+  const theme = createTheme({
+    palette: {
+      primary: purple,
+      secondary: green,
+    },
+    typography: {
+      h1: {
+        fontSize: "10em",
+        fontFamily: "Raleway, Arial",
+      },
+    },
+  });
   return (
-    <Grid item container direction="column">
-      <Coolbutton cool={cool}></Coolbutton>
-      <Buttons />
-    </Grid>
+    <ThemeProvider theme={theme}>
+      <Grid item container direction="column">
+        <Coolbutton cool={cool}></Coolbutton>
+        <Buttons />
+        <Typographyexapmles />
+      </Grid>
+    </ThemeProvider>
   );
 }
---
+++ make-styles/src/Typographyexapmles.jsx
@@ -0,0 +1,31 @@
+import { makeStyles } from '@material-ui/core';
+import React from 'react';
+import { Typography } from '@mui/material';
+
+const useStyle = makeStyles({
+    typographyStyle: {
+        color: "blue",
+    },
+    gutterBottom:{
+        marginBottom: '0.2em'
+    }
+})
+
+export default function Typographyexapmles() {
+    const classes = useStyle()
+    return (
+        <Typography 
+        //className={classes.typographyStyle}
+        //fontFamily="sans-serif"
+        //fontSize='8em'
+        //align='right'
+        //color='secondary'
+        //gutterBottom
+        //classes={{ gutterBottom: classes.gutterBottom }}
+        variant='h1'
+        color='secondary'
+        >
+            xx
+        </Typography>
+    );
+}
